7274492cd083b4324897e0f51f624b1940beec6f
Louisa Hunt <louisa.m.ross@gmail.com>
2014-03-02 20:44:03 -0800

    making it a little neater

--- strings_and_things.rb
+++ strings_and_things.rb
@@ -1,11 +1,10 @@
 print "do you have a dog?"
 user_input = gets.chomp.downcase
 
-
 if user_input.include? "s"
-user_input.gsub!(/s/, "th")
+  user_input.gsub!(/s/, "th")
 else
-    puts "none found"
+  puts "none found"
 end
 
 puts "your string is: #{user_input}"

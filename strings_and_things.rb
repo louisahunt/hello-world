9845f2b7f2f4dc37bd14eae3c95149437fc51ee1
Louisa Hunt <louisa.m.ross@gmail.com>
2014-03-02 20:36:33 -0800

    strings_and_things
    Day 2 brought to you by http://www.codecademy.com/
    
    Thanks :)

---
+++ strings_and_things.rb
@@ -0,0 +1,11 @@
+print "do you have a dog?"
+user_input = gets.chomp.downcase
+
+
+if user_input.include? "s"
+user_input.gsub!(/s/, "th")
+else
+    puts "none found"
+end
+
+puts "your string is: #{user_input}"

f8483cc9769e6a1fad5f2c108d6e1f5298a91162
Louisa Hunt <louisa.m.ross@gmail.com>
2014-03-08 18:17:51 -0800

    redacted_bunny
    takes the bunny out of “hello bunny muffin”

---
+++ redacted_bunny.rb
@@ -0,0 +1,14 @@
+puts "Text input: "
+text = gets.chomp
+puts "Word to redact: "
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+  if word != redact
+    print word + " "
+  else
+    print "REDACTED "
+  end
+end

336fe994aec0ac3576e1bbb586ad763b3142bc08
Louisa Hunt <louisa.m.ross@gmail.com>
2014-03-09 16:54:24 -0700

    data_structure_make_me_hungry
    trying out arrays while dinner is cooking, please feed me.

---
+++ data_structures_before_dinner.rb
@@ -0,0 +1,10 @@
+too_hungry = {
+  "starter" => "tomato basil flatbread",
+  "first"   => "mac n' cheese",
+  "main"    => "london broil and roasted veggies",
+  "dessert" => "chocolate cake"
+}
+
+too_hungry.each do |title, food|
+  puts food
+end
